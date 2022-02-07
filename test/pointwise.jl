bbd9924e383bc39362fdc3e58ce2e05d7da880e0
Júlio Hoffimann <julio.hoffimann@gmail.com>
2022-02-07 11:37:00 -0300

    Updates for latest deps

--- test/pointwise.jl
+++ test/pointwise.jl
@@ -1,19 +1,19 @@
 @testset "PointwiseLearn" begin
+  rng = MersenneTwister(1234)
   # synthetic data
-  Random.seed!(1234)
   f(x,y) = sin(4*(abs(x)+abs(y))) < 0 ? 1 : 0 
   X = [sin(i/10) for i in 1:100, j in 1:100]
   Y = [sin(j/10) for i in 1:100, j in 1:100]
   Z = categorical(f.(X,Y))
-  ϵ₁ = 0.1randn(Float64, size(X))
-  ϵ₂ = 0.1randn(Float64, size(Y))
+  ϵ₁ = 0.1randn(rng, Float64, size(X))
+  ϵ₂ = 0.1randn(rng, Float64, size(Y))
 
   # source and target data
   S = georef((X=X,Y=Y,Z=Z))
   T = georef((X=X+ϵ₁,Y=Y+ϵ₂))
 
   # view versions
-  inds = shuffle(1:nelements(S))
+  inds = shuffle(rng, 1:nelements(S))
   Sv = view(S, inds)
   Tv = view(T, inds)
 
